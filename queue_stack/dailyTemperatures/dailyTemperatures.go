934402b798c7784f448baebf06a6f893fd8afb52
Wang Kirin <wangqilintech@gmail.com>
2020-02-18 17:39:03 +0800

    modify

--- queue_stack/dailyTemperatures/dailyTemperatures.go
+++ queue_stack/dailyTemperatures/dailyTemperatures.go
@@ -1,7 +1,5 @@
 package dailyTemperatures
 
-import "fmt"
-
 //No.739
 //根据每日 气温 列表，请重新生成一个列表，对应位置的输入是你需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。
 //
@@ -9,18 +7,21 @@ import "fmt"
 //
 //提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数
 
-
 func dailyTemperatures(T []int) []int {
-	Out:=make([]int,len(T))
-	for i:=0;i<len(T); i++{
-		for j:=i+1;j<len(T);j++  {
-			fmt.Printf("i=%d, j=%d,len(T)=%d\n",i,j,len(T))
-			if T[j]>T[i] {
-				Out[i]=(j-i)
-				break
-			}
-		}
-	}
-	return Out
 
 }
+
+//// 老土解法：暴力解
+//func dailyTemperatures(T []int) []int {
+//	Out:=make([]int,len(T))
+//	for i:=0;i<len(T); i++{
+//		for j:=i+1;j<len(T);j++  {
+//			if T[j]>T[i] {
+//				Out[i]=(j-i)
+//				break
+//			}
+//		}
+//	}
+//	return Out
+//
+//}

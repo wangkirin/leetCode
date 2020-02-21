f78c720587dd2b416b763924f7b04374f44433a5
Wang Kirin <wangqilintech@gmail.com>
2020-02-21 15:19:26 +0800

    add sigle stack solution

--- queue_stack/dailyTemperatures/dailyTemperatures.go
+++ queue_stack/dailyTemperatures/dailyTemperatures.go
@@ -1,5 +1,7 @@
 package dailyTemperatures
 
+import "container/list"
+
 //No.739
 //根据每日 气温 列表，请重新生成一个列表，对应位置的输入是你需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。
 //
@@ -8,8 +10,52 @@ package dailyTemperatures
 //提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数
 
 func dailyTemperatures(T []int) []int {
-
+	res := make([]int, len(T), len(T))
+	stack := list.New()
+	for i, num := range T {
+		for stack.Len() != 0 && T[stack.Front().Value.(int)] < num {
+			ind := stack.Remove(stack.Front()).(int)
+			res[ind] = i-ind
+		}
+		stack.PushFront(i)
+	}
+	return res
 }
+
+//https://leetcode-cn.com/problems/daily-temperatures/solution/golangshi-yong-containerlistbao-by-yang7y7y7/
+
+
+
+
+//func dailyTemperatures(T []int) []int {
+//	Out:=make([]int,len(T))
+//	stack:=[]int{}
+//	for i:= len(T)-1;i>=0 ;i--{
+//		lt:=len(stack)
+//		if lt==0 {
+//			Out[i]=0
+//			stack=append(stack,i)
+//			continue
+//		}
+//		if T[stack[lt-1]]>T[i] {
+//			Out[i]= stack[lt-1]-i
+//			stack=append(stack,i)
+//
+//		}else {
+//			for j:=lt-1;j>=0 ;j--  {
+//				if (T[stack[j]]>T[i]){
+//					Out[i]= stack[j]-i
+//					break
+//				}else {
+//					stack=stack[0:len(stack)-1]
+//					stack=append(stack,i)
+//				}
+//			}
+//
+//		}
+//	}
+//	return Out
+//}
 
 //// 老土解法：暴力解
 //func dailyTemperatures(T []int) []int {

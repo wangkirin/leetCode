43bf30c491cd3f4b2f3d763d7fa22f919b89cc5b
Wang Qilin <qilin.wang@huawei.com>
2022-05-12 15:10:31 +0800

    update

--- link-list/fu-za-lian-biao-de-fu-zhi-lcof.go
+++ link-list/fu-za-lian-biao-de-fu-zhi-lcof.go
@@ -6,7 +6,40 @@ type Node struct {
 	Random *Node
 }
 
+//拆分+拼接
 func copyRandomList(head *Node) *Node {
+	if head == nil {
+		return nil
+	}
+	cur := head
+	for cur != nil {
+		n := &Node{Val: cur.Val}
+		n.Next = cur.Next
+		cur.Next = n
+		cur = cur.Next.Next
+	}
+	cur = head
+	for cur != nil {
+		if cur.Random == nil {
+			cur.Next.Random = nil
+		} else {
+			cur.Next.Random = cur.Random.Next
+		}
+		cur = cur.Next.Next
+	}
+	newHead := head.Next
+	for node := head; node != nil; node = node.Next {
+		nodeNew := node.Next
+		node.Next = node.Next.Next
+		if nodeNew.Next != nil {
+			nodeNew.Next = nodeNew.Next.Next
+		}
+	}
+	return newHead
+}
+
+//哈希
+func copyRandomList1(head *Node) *Node {
 	if head == nil {
 		return nil
 	}
